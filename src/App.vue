03678b23860b4474b23d3fc795e1f518a9a6a0ac
Henrique <henryqueoli@hotmail.com>
2021-11-06 11:44:59 -0300

    feat: add tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,9 @@
 <template>
     <router-view />
 </template>
+
+<style lang="postcss">
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    purge: [],
+    darkMode: "class",
+    theme: {
+        extend: {
+            transitionProperty: {
+                width: "width",
+            },
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
