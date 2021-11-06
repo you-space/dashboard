d2920ba20efae1e19c95993bd89353167df3cb0a
Henrique <henryqueoli@hotmail.com>
2021-11-06 12:53:20 -0300

    feat: add vite alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import path from "path";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
@@ -5,24 +6,15 @@ import vue from "@vitejs/plugin-vue";
 export default defineConfig({
     plugins: [vue()],
     resolve: {
-        alias: [
-            {
-                find: "assets",
-                replacement: "/api/v1/assets",
-            },
-            {
-                find: "pages",
-                replacement: "/api/v1/admin/dashboard/pages",
-            },
-        ],
+        alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
     },
-    server: {
-        proxy: {
-            "^/api/.*": {
-                target: "http://localhost:3333/api/",
-                changeOrigin: true,
-                rewrite: (path) => path.replace(/^\/api/, ""),
-            },
-        },
-    },
+    // server: {
+    //     proxy: {
+    //         "^/api/.*": {
+    //             target: "http://localhost:3333/api/",
+    //             changeOrigin: true,
+    //             rewrite: (path) => path.replace(/^\/api/, ""),
+    //         },
+    //     },
+    // },
 });
