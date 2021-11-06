4676523d79c588413f2b50c9a9a9757a0e78dcd3
Henrique <henryqueoli@hotmail.com>
2021-11-06 17:10:16 -0300

    feat: create primary colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
     purge: [],
     darkMode: "class",
     theme: {
+        colors: {
+            ...colors,
+            primary: "var(--color-primary)",
+        },
         extend: {
             transitionProperty: {
                 width: "width",
