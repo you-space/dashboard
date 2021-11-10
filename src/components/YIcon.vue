979f55dc36191856f7443b4312e7fd5504857c7f
Henrique <henryqueoli@hotmail.com>
2021-11-10 13:57:40 -0300

    feat: add font-awesome icons

---
+++ src/components/YIcon.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { AppContext } from "@vue/runtime-core";
+
+library.add(fas);
+
+defineProps({
+    name: {
+        type: String,
+        required: true,
+    },
+    clickable: {
+        type: Boolean,
+        default: false,
+    },
+});
+</script>
+<template>
+    <font-awesome-icon
+        :icon="name"
+        :class="clickable ? 'cursor-pointer' : ''"
+    />
+</template>
