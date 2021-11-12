5ccfbefb07094985db1a293980753cdae75d38f0
Henrique <henryqueoli@hotmail.com>
2021-11-12 09:25:17 -0300

    feat: add you-space logo

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,5 +1,13 @@
+<script setup lang="ts">
+// import { ref } from 'vue'
+import Logo from "@/assets/logo.svg";
+</script>
+
 <template>
-    <div class="flex">
-        <y-card> Home page </y-card>
+    <div class="flex items-center justify-center h-full w-full">
+        <div class="text-center">
+            <img :src="Logo" :alt="$store.state.app.name" class="mb-4" />
+            <div>Home page</div>
+        </div>
     </div>
 </template>
