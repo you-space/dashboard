80ec5b6736bb58c033b2da13d979e73448d693ba
Henrique <henryqueoli@hotmail.com>
2021-11-12 14:42:25 -0300

    feat: add YProgressBar component

---
+++ src/components/YProgressBar.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="y-progress-bar"></div>
+</template>
+<style lang="scss">
+@keyframes y-progress-bar-width {
+    0% {
+        margin-left: 0;
+        width: 0;
+    }
+    50% {
+        margin-left: 50%;
+        width: 50%;
+    }
+    100% {
+        margin-left: 100%;
+        width: 0%;
+    }
+}
+
+.y-progress-bar {
+    @apply bg-primary;
+
+    --height: 2px;
+    width: 30%;
+
+    height: var(--height);
+    animation: y-progress-bar-width 2s ease-in-out infinite;
+}
+</style>
