59633a7a8998b9100876854029e490b08cdc9d89
Henrique <henryqueoli@hotmail.com>
2021-11-17 18:05:53 -0300

    feat: add outlined style to y-btn

--- src/components/YBtn.vue
+++ src/components/YBtn.vue
@@ -1,5 +1,28 @@
 <template>
-    <button class="yt-btn" v-bind="$attrs" :class="classes">
+    <router-link
+        v-if="to"
+        tag="button"
+        class="yt-btn"
+        v-bind="$attrs"
+        :class="classes"
+        :to="to"
+    >
+        <template v-if="label">
+            {{ label }}
+        </template>
+
+        <slot v-else />
+
+        <div
+            v-if="loading"
+            class="absolute inset-0 flex items-center justify-center"
+            :class="`bg-${color}`"
+        >
+            <!-- <yt-spin :class="`text-${textColor}`" /> -->
+        </div>
+    </router-link>
+
+    <button v-else class="yt-btn" v-bind="$attrs" :class="classes">
         <template v-if="label">
             {{ label }}
         </template>
@@ -17,6 +40,7 @@
 </template>
 <script lang="ts" setup>
 import { PropType, computed } from "vue";
+import { RouterLinkProps } from "vue-router";
 
 const props = defineProps({
     label: {
@@ -39,6 +63,18 @@ const props = defineProps({
         type: String as PropType<"sm" | "md">,
         default: "md",
     },
+    to: {
+        type: [Object, String],
+        default: null,
+    },
+    outlined: {
+        type: Boolean,
+        default: false,
+    },
+    rounded: {
+        type: Boolean,
+        default: true,
+    },
 });
 
 const sizes = {
@@ -46,17 +82,26 @@ const sizes = {
     md: "py-2 px-4 text-sm",
 };
 
-const classes = computed(() => [
-    "uppercase",
-    `bg-${props.color}`,
-    `text-${props.textColor}`,
-    sizes[props.size] || sizes.md,
-]);
+const classes = computed(() => {
+    const result = ["uppercase", sizes[props.size] || sizes.md];
+
+    if (props.rounded) {
+        result.push("rounded");
+    }
+
+    if (props.outlined) {
+        result.push(`border border-${props.color} text-${props.color}`);
+    } else {
+        result.push(`bg-${props.color} text-${props.textColor}`);
+    }
+
+    return result;
+});
 </script>
 <style lang="scss">
 .yt-btn {
     @apply relative;
-    @apply rounded cursor-pointer;
+    @apply cursor-pointer;
     @apply focus:outline-none border;
     @apply font-bold;
 }
