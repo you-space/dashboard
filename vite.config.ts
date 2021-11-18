972aa3efa35543181aa47979e3aa20bcc9eb1edb
Henrique <henryqueoli@hotmail.com>
2021-11-17 22:17:32 -0300

    feat: add api proxy connection methods

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,28 @@
 import path from "path";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [vue()],
-    resolve: {
-        alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
-    },
-    // server: {
-    //     proxy: {
-    //         "^/api/.*": {
-    //             target: "http://localhost:3333/api/",
-    //             changeOrigin: true,
-    //             rewrite: (path) => path.replace(/^\/api/, ""),
-    //         },
-    //     },
-    // },
+export default defineConfig(({ mode }) => {
+    console.log(loadEnv(mode, process.cwd()));
+
+    const { API_PROXY_URL } = loadEnv(mode, process.cwd());
+
+    const proxyUrl = API_PROXY_URL || "http://localhost:3333";
+
+    return {
+        plugins: [vue()],
+        resolve: {
+            alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+        },
+        server: {
+            proxy: {
+                "^/api/.*": {
+                    target: `${proxyUrl}/api`,
+                    changeOrigin: true,
+                    rewrite: (path) => path.replace(/^\/api/, ""),
+                },
+            },
+        },
+    };
 });
