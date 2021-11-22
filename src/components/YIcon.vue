a1b5e674737dc4aff6d22ce75ee6b48bb1ebe5fc
Henrique <henryqueoli@hotmail.com>
2021-11-22 19:41:40 -0300

    feat: add clickable hover styles in y-icon

--- src/components/YIcon.vue
+++ src/components/YIcon.vue
@@ -2,11 +2,11 @@
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { AppContext } from "@vue/runtime-core";
+import { computed, PropType } from "vue";
 
 library.add(fas);
 
-defineProps({
+const props = defineProps({
     name: {
         type: String,
         required: true,
@@ -15,13 +15,41 @@ defineProps({
         type: Boolean,
         default: false,
     },
+    size: {
+        type: String as PropType<"md">,
+        default: "md",
+    },
+});
+
+const classes = computed(() => {
+    const sizes = {
+        md: "h-8 w-8 text-base",
+    };
+
+    const result = [
+        "y-icon",
+        props.clickable ? "y-icon-clickable" : "",
+        sizes[props.size] || sizes.md,
+    ];
+
+    return result;
 });
 </script>
 <template>
-    <i>
-        <font-awesome-icon
-            :icon="name"
-            :class="clickable ? 'cursor-pointer' : ''"
-        />
+    <i :class="classes">
+        <font-awesome-icon :icon="name" />
     </i>
 </template>
+
+<style lang="scss">
+.y-icon {
+    @apply flex items-center justify-center;
+    @apply rounded-full text-sm;
+
+    &.y-icon-clickable {
+        @apply cursor-pointer;
+        @apply transition-colors;
+        @apply hover:bg-gray-200;
+    }
+}
+</style>
