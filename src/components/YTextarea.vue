f9c387f865df307aba8507fb68bfc8ecf79ddb95
Henrique <henryqueoli@hotmail.com>
2021-12-17 15:47:22 -0300

    feat: add y-textarea component

---
+++ src/components/YTextarea.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { useModel } from "@/compositions/helpers";
+import { computed } from "vue";
+
+const props = defineProps({
+    modelValue: {
+        type: String,
+        default: "",
+    },
+    minHeight: {
+        type: Number,
+        default: 0,
+    },
+});
+
+const style = computed(() => ({
+    minHeight: `${props.minHeight}px`,
+}));
+const emit = defineEmits(["update:modelValue"]);
+
+const model = useModel(props, "modelValue", emit);
+</script>
+<template>
+    <y-input v-model="model">
+        <template #input="{ attrs }">
+            <textarea v-model="model" :style="style" v-bind="attrs"></textarea>
+        </template>
+    </y-input>
+</template>
